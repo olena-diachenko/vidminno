be7d6e31c9c68e9e56f6976495fe955e07173786
Olena Diachenko <ellenka24@gmail.com>
2023-10-19 16:07:49 +0300

    added redux-persist, react-charts-2 libraries

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.jsx';
 import './assets/scss/index.scss';
 import 'rsuite/dist/rsuite.min.css';
-import store from './store';
+import store, { persistor } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <PersistGate loading={null} persistor={persistor}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </PersistGate>
     </Provider>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,52 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { usersApi } from './usersApi';
 import popUpReducer from './slices/popUp';
 import themeReducer from './slices/theme';
+import authReducer from './slices/auth';
 
-const store = configureStore({
-    reducer: {
-        [usersApi.reducerPath]: usersApi.reducer,
-        popUp: popUpReducer,
-        theme: themeReducer,
-    },
-
-    middleware: getDefaultMiddleware =>
-        getDefaultMiddleware().concat(usersApi.middleware),
+const rootReducer = combineReducers({
+    [usersApi.reducerPath]: usersApi.reducer,
+    popUp: popUpReducer,
+    theme: themeReducer,
+    auth: authReducer,
 });
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: [usersApi.reducerPath],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }).concat(usersApi.middleware),
+});
+
+export const persistor = persistStore(store);
 export default store;
