0afd5c2c65db00d922db74e23812dfe6e08dd4cd
Olena Diachenko <ellenka24@gmail.com>
2023-10-23 13:20:07 +0300

    added Slider component

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Carousel } from 'rsuite';
+
+const Slider = props => {
+    const data = props.videos;
+
+    return (
+        <Carousel className="custom-slider" shape="bar">
+            {data.map(video => (
+                <iframe
+                    key={video.id}
+                    src={video.src}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen
+                ></iframe>
+            ))}
+        </Carousel>
+    );
+};
+
+export default Slider;
