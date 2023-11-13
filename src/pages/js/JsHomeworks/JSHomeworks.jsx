c6157befc1fb0eacc181687a606f829a47c71b22
Olena Diachenko <ellenka24@gmail.com>
2023-11-13 21:23:14 +0200

    added JSHomework page

--- src/pages/js/JsHomeworks/JSHomeworks.jsx
+++ src/pages/js/JsHomeworks/JSHomeworks.jsx
@@ -1,3 +1,27 @@
-const JSHomeworks = () => {};
+import { Loader } from 'rsuite';
+import { useSelector } from 'react-redux';
+import DefaultTemplate from '../../../templates/defaultPage';
+import { useGetJsHomeworksQuery } from '../../../store/api';
+import Homeworks from '../../../components/Homeworks';
+
+const JSHomeworks = () => {
+    const { data: homeworks, isLoading } = useGetJsHomeworksQuery();
+    const user = useSelector(state => state.auth.user);
+
+    return (
+        <DefaultTemplate>
+            {isLoading ? (
+                <Loader center={true} size="lg" speed="slow" />
+            ) : (
+                <Homeworks
+                    homeworks={homeworks}
+                    path={'/js-course/homeworks/'}
+                    course={'JS Course'}
+                    user={user}
+                />
+            )}
+        </DefaultTemplate>
+    );
+};
 
 export default JSHomeworks;
