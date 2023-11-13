afe247830797391fafbb8b743c86e1bcb4e28371
Olena Diachenko <ellenka24@gmail.com>
2023-11-13 21:29:18 +0200

    added ReactHomework page

--- src/pages/react/ReactHomeworks/ReactHomeworks.jsx
+++ src/pages/react/ReactHomeworks/ReactHomeworks.jsx
@@ -1,3 +1,27 @@
-const ReactHomeworks = () => {};
+import { Loader } from 'rsuite';
+import { useSelector } from 'react-redux';
+import DefaultTemplate from '../../../templates/defaultPage';
+import { useGetReactHomeworksQuery } from '../../../store/api';
+import Homeworks from '../../../components/Homeworks';
+
+const ReactHomeworks = () => {
+    const { data: homeworks, isLoading } = useGetReactHomeworksQuery();
+    const user = useSelector(state => state.auth.user);
+
+    return (
+        <DefaultTemplate>
+            {isLoading ? (
+                <Loader center={true} size="lg" speed="slow" />
+            ) : (
+                <Homeworks
+                    homeworks={homeworks}
+                    path={'/react-course/homeworks/'}
+                    course={'React Course'}
+                    user={user}
+                />
+            )}
+        </DefaultTemplate>
+    );
+};
 
 export default ReactHomeworks;
