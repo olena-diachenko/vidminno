c5674d14fb5abc38a6b147d89c4d2dc7414dfcb5
Olena Diachenko <ellenka24@gmail.com>
2023-11-16 13:49:29 +0200

    changed "is completed homework" logic

--- src/pages/js/JsHomeworks/JSHomeworks.jsx
+++ src/pages/js/JsHomeworks/JSHomeworks.jsx
@@ -1,12 +1,20 @@
 import { Loader } from 'rsuite';
 import { useSelector } from 'react-redux';
 import DefaultTemplate from '../../../templates/defaultPage';
-import { useGetJsHomeworksQuery } from '../../../store/api';
+import {
+    useGetJsHomeworksByUserIdQuery,
+    useGetJsHomeworksQuery,
+    useGetUserByNameQuery,
+} from '../../../store/api';
 import Homeworks from '../../../components/Homeworks';
 
 const JSHomeworks = () => {
     const { data: homeworks, isLoading } = useGetJsHomeworksQuery();
     const user = useSelector(state => state.auth.user);
+    const { data: student, isLoading: isLoad } = useGetUserByNameQuery(user);
+    const studentId = !isLoad && student[0].id;
+    const { data: userHomeworks, isLoading: isLoadHw } =
+        useGetJsHomeworksByUserIdQuery(studentId);
 
     return (
         <DefaultTemplate>
@@ -17,7 +25,7 @@ const JSHomeworks = () => {
                     homeworks={homeworks}
                     path={'/js-course/homeworks/'}
                     course={'JS Course'}
-                    user={user}
+                    userHomeworks={!isLoadHw && userHomeworks}
                 />
             )}
         </DefaultTemplate>
