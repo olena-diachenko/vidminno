be01e5d6ec7bf56c5fe1a27c24951ffb08cc1c22
Olena Diachenko <ellenka24@gmail.com>
2023-11-27 22:13:02 +0200

    added "Views" block

--- src/pages/Articles/TechnicalArticles/TechnicalArticles.jsx
+++ src/pages/Articles/TechnicalArticles/TechnicalArticles.jsx
@@ -26,6 +26,7 @@ import {
 } from 'react-icons/fa';
 import TagNumberIcon from '@rsuite/icons/TagNumber';
 import { useSelector } from 'react-redux';
+import VisibleIcon from '@rsuite/icons/Visible';
 import DefaultTemplate from '../../../templates/defaultPage';
 import { useGetTechArticlesQuery } from '../../../store/api';
 import styles from './style.module.scss';
@@ -34,13 +35,6 @@ const TechnicalArticles = () => {
     const { data: articles, isLoading } = useGetTechArticlesQuery();
     const navigation = useNavigate();
     const theme = useSelector(state => state.theme.theme);
-    const redirectHandler = value => () => {
-        navigation(`/technical-articles/${value}`);
-    };
-
-    const clickHandler = e => {
-        console.log(e);
-    };
 
     const TSIcon = React.forwardRef((props, ref) => (
         <svg
@@ -55,6 +49,14 @@ const TechnicalArticles = () => {
             <path d="M45,4H5C4.447,4,4,4.448,4,5v40c0,0.552,0.447,1,1,1h40c0.553,0,1-0.448,1-1V5C46,4.448,45.553,4,45,4z M29,26.445h-5V42h-4	V26.445h-5V23h14V26.445z M30.121,41.112v-4.158c0,0,2.271,1.712,4.996,1.712c2.725,0,2.62-1.782,2.62-2.026	c0-2.586-7.721-2.586-7.721-8.315c0-7.791,11.25-4.717,11.25-4.717l-0.14,3.704c0,0-1.887-1.258-4.018-1.258s-2.9,1.013-2.9,2.096	c0,2.795,7.791,2.516,7.791,8.141C42,44.955,30.121,41.112,30.121,41.112z"></path>
         </svg>
     ));
+
+    const redirectHandler = value => () => {
+        navigation(`/technical-articles/${value}`);
+    };
+
+    const clickHandler = e => {
+        console.log(e);
+    };
 
     return (
         <DefaultTemplate>
@@ -100,33 +102,69 @@ const TechnicalArticles = () => {
                                                 )}
                                         </div>
                                     </div>
-                                    <ButtonToolbar>
-                                        <ButtonGroup>
-                                            <IconButton
-                                                className={styles.panel__button}
-                                                size="md"
-                                                icon={
-                                                    <Icon
-                                                        as={FaHeart}
+                                    <div className={styles.panel__btnGroup}>
+                                        <ButtonToolbar>
+                                            <ButtonGroup>
+                                                <IconButton
+                                                    className={
+                                                        styles.panel__button
+                                                    }
+                                                    size="md"
+                                                    icon={
+                                                        <Icon
+                                                            as={FaHeart}
+                                                            style={{
+                                                                color: 'white',
+                                                            }}
+                                                        />
+                                                    }
+                                                />
+                                                <Button
+                                                    className={
+                                                        styles.panel__button
+                                                    }
+                                                    appearance="primary"
+                                                    size="md"
+                                                    endIcon={<PageNextIcon />}
+                                                    onClick={redirectHandler(
+                                                        index + 1
+                                                    )}
+                                                >
+                                                    Read
+                                                </Button>
+                                            </ButtonGroup>
+                                        </ButtonToolbar>
+                                        <div>
+                                            <ButtonToolbar>
+                                                <ButtonGroup>
+                                                    <IconButton
+                                                        className={
+                                                            styles.panel__viewButton
+                                                        }
+                                                        size="md"
+                                                        icon={
+                                                            <Icon
+                                                                as={VisibleIcon}
+                                                                style={{
+                                                                    color: 'white',
+                                                                }}
+                                                                className={
+                                                                    styles.panel__viewButton
+                                                                }
+                                                            />
+                                                        }
                                                         style={{
                                                             color: 'white',
+                                                            paddingLeft: 40,
                                                         }}
-                                                    />
-                                                }
-                                            />
-                                            <Button
-                                                className={styles.panel__button}
-                                                appearance="primary"
-                                                size="md"
-                                                endIcon={<PageNextIcon />}
-                                                onClick={redirectHandler(
-                                                    index + 1
-                                                )}
-                                            >
-                                                Read
-                                            </Button>
-                                        </ButtonGroup>
-                                    </ButtonToolbar>
+                                                        disabled
+                                                    >
+                                                        Views
+                                                    </IconButton>
+                                                </ButtonGroup>
+                                            </ButtonToolbar>
+                                        </div>
+                                    </div>
                                 </Panel>
                             ))}
                         </PanelGroup>
